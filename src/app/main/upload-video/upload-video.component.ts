c5f9da2e8f44d5c164443c0f661c05b17c9d37d6
Paula Sierra <Paulacode01@gmail.com>
2021-02-02 20:48:05 -0500

    Agregando drop y botón elminar para array de archivos

--- src/app/main/upload-video/upload-video.component.ts
+++ src/app/main/upload-video/upload-video.component.ts
@@ -15,7 +15,7 @@ export class UploadVideoComponent implements OnInit {
   url;
   constructor(private sanitizer:DomSanitizer,private formBuilder:FormBuilder,private videoService:VideoService) { }
   //creamos un array para guardar los videos
-  myFiles:string;
+  myFiles:string|File;
   myVideos:Video;
   enviado:boolean=false;
 
@@ -53,50 +53,60 @@ export class UploadVideoComponent implements OnInit {
     //llamamos el servicio
     
   }
+  public dropped(files: NgxFileDropEntry[]) {
+    this.files = files;
+    
+    // if (this.files[0].fileEntry.isFile) {
+    //   const fileEntry = this.files[0].fileEntry as FileSystemFileEntry;
+    //   fileEntry.file((file: File) => {
+    //     console.log("Ento al if: ", file);
+    //     this.myFiles[0]=file;
+    //   });
+    // }
 
-
+    // for (const droppedFile of files) {
+    //   // Is it a file?
+    //   if (droppedFile.fileEntry.isFile) {
+    //     const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+    //     fileEntry.file((file: File) => {
+ 
+    //       // Here you can access the real file
+    //       console.log("Ento al if: ",droppedFile.relativePath, file);
+          
+    //       /**
+    //       // You could upload it like this:
+    //       const formData = new FormData()
+    //       formData.append('logo', file, relativePath)
+ 
+    //       // Headers
+    //       const headers = new HttpHeaders({
+    //         'security-token': 'mytoken'
+    //       })
+ 
+    //       this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
+    //       .subscribe(data => {
+    //         // Sanitized logo returned from backend
+    //       })
+    //       **/
+ 
+    //     });
+    //   } else {
+    //     // It was a directory (empty directories are added, otherwise only files)
+    //     const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+    //     console.log(droppedFile.relativePath, fileEntry);
+    //   }
+    // }
+  }
+  public fileOver(event:Event){
+    console.log(event);
+  }
+ 
+  public fileLeave(event:Event){
+    console.log(event);
+  }
+  public deleteVideo():void{
+    this.files=[];
+  }
 }
 
-  // public dropped(files: NgxFileDropEntry[]) {
-  //   this.files = files;
-  //   for (const droppedFile of files) {
- 
-  //     // Is it a file?
-  //     if (droppedFile.fileEntry.isFile) {
-  //       const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
-  //       fileEntry.file((file: File) => {
- 
-  //         // Here you can access the real file
-  //         console.log("Ento al if: ",droppedFile.relativePath, file);
-          
-  //         /**
-  //         // You could upload it like this:
-  //         const formData = new FormData()
-  //         formData.append('logo', file, relativePath)
- 
-  //         // Headers
-  //         const headers = new HttpHeaders({
-  //           'security-token': 'mytoken'
-  //         })
- 
-  //         this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
-  //         .subscribe(data => {
-  //           // Sanitized logo returned from backend
-  //         })
-  //         **/
- 
-  //       });
-  //     } else {
-  //       // It was a directory (empty directories are added, otherwise only files)
-  //       const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
-  //       console.log(droppedFile.relativePath, fileEntry);
-  //     }
-  //   }
-  // }
-  // public fileOver(event:Event){
-  //   console.log(event);
-  // }
- 
-  // public fileLeave(event:Event){
-  //   console.log(event);
-  // }
+  
