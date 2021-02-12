576568a5d03334b34beeeff091df588a33d9249b
Paula Sierra <Paulacode01@gmail.com>
2021-02-12 00:03:23 -0500

    Creación de carousel

--- src/app/events/components/event/event.component.ts
+++ src/app/events/components/event/event.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Route } from '@angular/compiler/src/core';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
@@ -12,6 +13,7 @@ export class EventComponent implements OnInit {
   constructor(private router:Router) { }
 
   ngOnInit(): void {
+    
   }
 
 }
--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { EventsRoutingModule } from './events-routing.module';
 import { EventComponent } from './components/event/event.component';
 import { EventDetailComponent } from './components/event-detail/event-detail.component';
-import { EventCarrouselComponent } from './components/event-carrousel/event-carrousel.component';
-
 
 @NgModule({
-  declarations: [EventComponent, EventDetailComponent, EventCarrouselComponent],
+  declarations: [EventComponent, EventDetailComponent],
   imports: [
     CommonModule,
-    EventsRoutingModule
+    EventsRoutingModule,
   ]
 })
 export class EventsModule { }
